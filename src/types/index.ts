fc66d1b080758bb9430cc0e8195de45a41ad1458
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-04-27 16:49:44 +0200

    removed partial from MockgooseModel type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,13 @@
 import type mongoose from 'mongoose';
 
-export interface MockgooseModel extends Partial<
-  mongoose.Model<any, unknown, unknown, unknown, any, any, unknown>
+export interface MockgooseModel extends mongoose.Model<
+  any,
+  unknown,
+  unknown,
+  unknown,
+  any,
+  any,
+  unknown
 > {}
 
 export type DBState = Record<string, any>;
